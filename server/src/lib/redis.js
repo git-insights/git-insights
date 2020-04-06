08535c16c0222ce3f3f2a569f0a9f3f9b3742cdc
Murat Sutunc <sutunc@gmail.com>
2020-04-05 23:41:19 -0700

    better backoff str for redis

--- server/src/lib/redis.js
+++ server/src/lib/redis.js
@@ -1,20 +1,27 @@
 const Redis = require('ioredis');
 
-const client = new Redis({
+const options = {
   port: process.env.REDIS_PORT,
   host: process.env.REDIS_HOSTNAME,
   family: 4, // 4 (IPv4) or 6 (IPv6)
   password: process.env.REDIS_PASSWORD,
   db: 0,
-});
+  connectTimeout: 17000,
+  maxRetriesPerRequest: 4,
+  retryStrategy: (times) => Math.min(times * 30, 1000),
+  reconnectOnError: (error) => {
+    const targetErrors = [/READONLY/, /ETIMEDOUT/];
 
-const subscriber = new Redis({
-  port: process.env.REDIS_PORT,
-  host: process.env.REDIS_HOSTNAME,
-  family: 4, // 4 (IPv4) or 6 (IPv6)
-  password: process.env.REDIS_PASSWORD,
-  db: 0,
-});
+    targetErrors.forEach((targetError) => {
+      if (targetError.test(error.message)) {
+        return true;
+      }
+    });
+  }
+}
+
+const client = new Redis(options);
+const subscriber = new Redis(options);
 
 module.exports = {
   client,
